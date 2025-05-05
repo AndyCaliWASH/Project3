5e02eedecf6616eece00c9d31cf81def68c3a732
Andres Valencia Cardenas <drevcardenas27@gmail.com>
2025-05-04 18:46:12 -0700

    Added main.cpp and SFML

---
+++ Project3/main.cpp
@@ -0,0 +1,15 @@
+/*
+* Name: Andres Valencia Cardenas
+* SFML projects
+* Date: 05/04/2025
+*/
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+int main(void)
+{
+	std::cout << "Hello World" << std::endl; 
+	return 0; 
+}
