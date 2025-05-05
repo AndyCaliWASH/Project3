561109d8c4b78a9a9a15d8de59bcf6cbf825e5bb
Andres Valencia Cardenas <drevcardenas27@gmail.com>
2025-05-04 19:25:16 -0700

    Added a window and a yellow circle

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -10,6 +10,24 @@
 
 int main(void)
 {
-	std::cout << "Hello World" << std::endl; 
+	sf::RenderWindow window(sf::VideoMode({ 1920, 1080 }), "Game 01");
+	sf::CircleShape shape(500.f); 
+
+	shape.setFillColor(sf::Color::Yellow); 
+
+	while (window.isOpen())
+	{
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+			{
+				window.close();
+			}
+		}
+			window.clear();
+			window.draw(shape);
+			window.display();
+		
+	}
 	return 0; 
 }
